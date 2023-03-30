028f4a9738762b43be437a68a037ad317dea55df
ArathyPA <arathyajitp@gmail.com>
2023-03-30 13:15:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { addUser,deleteUser,editUser} from './features/Users';
+import { useDispatch } from 'react-redux';
+import maleprofile from './images/maleProfile.jpg';
+import femaleprofile from './images/femaleProfile.jpg';
+import Header from './Header';
+import Search from './Search';
+import Footer from './Footer';
+import StatisticsCard from './StatisticsCard';
+import Filter from './Filter';
+
 
 function App() {
+   const dispatch=useDispatch();
+  const userList=useSelector((state)=>state.users.value)
+  const [name,setName]=useState("");
+  const [contactType,setcontactType]=useState("");
+  const [gender,setGender]=useState("");
+  const [newName,setNewname]=useState("");
+  const [newcontactType,setNewcontactType]=useState("");
+  const [newGender,setNewGender]=useState("");
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Search dispatch={dispatch}/>
+      <StatisticsCard userList={userList}/>
+      <div className='addUser'>       
+        <input type="text" placeholder='Enter a name' onChange={(e)=>{setName(e.target.value)}}/>
+        <input type="text" placeholder='Enter contact type' onChange={(e)=>{setcontactType(e.target.value)}}/>
+        <input type="text" placeholder='Enter Gender' onChange={(e)=>{setGender(e.target.value)}}/>
+        <button onClick={()=>{dispatch(addUser({id:userList[userList.length-1].id+1,name,contactType,gender}))}}>Add New Contact</button>
+      </div>
+      
+      <Filter dispatch={dispatch}/>
+      
+      <div className='displayUser'>
+      {userList.map((user)=>{
+        return <div className="userCard"> 
+                 <div>{(user.gender.toLowerCase()==='male')?<img src={maleprofile} className="profileImg"/>:
+          <img src ={femaleprofile} className="profileImg"/>}</div>
+               <div>
+               <h1>{user.name}</h1>
+                 <h3>{user.contactType}</h3> <h4>{user.gender}</h4>
+                 <input type="text" placeholder='Enter name' onChange={(e)=>{setNewname(e.target.value)}}/>
+                 <input type="text" placeholder='Enter contact type' onChange={(e)=>{setNewcontactType(e.target.value)}}/>
+                 <input type="text" placeholder='Enter Gender' onChange={(e)=>{setNewGender(e.target.value)}}/>
+                 <button onClick={()=>{dispatch(editUser({id:user.id,name:newName,contactType:newcontactType,gender:newGender}))}}>Update</button>
+                 <button onClick={()=>{dispatch(deleteUser({id:user.id}))}}>Delete</button>
+               </div>
+                 </div>
+      })}
+      </div>
+      <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+import userReducer from './features/Users';
+const store=configureStore({
+  reducer:{
+       users:userReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
